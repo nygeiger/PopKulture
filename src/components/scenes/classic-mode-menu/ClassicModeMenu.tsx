52545b74d89ed09634874f2a43af30b237823a45
Nyles Geiger <nygeiger@gmail.com>
2026-02-06 00:28:49 -0500

    Refactor ClassicGameMenu component - change increase/decrease team button to select element

--- src/components/scenes/classic-mode-menu/ClassicModeMenu.tsx
+++ src/components/scenes/classic-mode-menu/ClassicModeMenu.tsx
@@ -1,13 +1,7 @@
-import { useState } from "react";
+import { useState, type BaseSyntheticEvent } from "react";
 import { SceneDict, type Team } from "../../../lib/definitions";
 import { MAX_TEAMS, POINTS_TO_WIN } from "../../../lib/utils";
 import "./ClassicModeMenu.css"
-
-
-export type ClassicGameMenuProps = {
-    handleChangeSceneButtonClick: (newSceneName: string) => void
-    setTeams: (teams: Team[]) => void
-}
 
 const createTeams = (numTeams: number): Team[] => {
     const teamArray: Team[] = []
@@ -20,8 +14,20 @@ const createTeams = (numTeams: number): Team[] => {
     return teamArray;
 }
 
+const teamOptions = Array.from({ length: MAX_TEAMS }, (_, index) => (<option key={index + 1} value={index + 1}>{index + 1}</option>));
+
+export type ClassicGameMenuProps = {
+    handleChangeSceneButtonClick: (newSceneName: string) => void
+    setTeams: (teams: Team[]) => void
+}
+
 export default function ClassicGameMenu(props: ClassicGameMenuProps) {
-    const [numTeams, setNumTeams] = useState(1);
+    const [numTeams, setNumTeams] = useState<number>(1);
+
+    const handleTeamsChange = (e: BaseSyntheticEvent) => {
+        setNumTeams(Number(e.target.value));
+    }
+
 
     //TODO: Display points to win via variable while adding necessary commas
     return (
@@ -30,10 +36,10 @@ export default function ClassicGameMenu(props: ClassicGameMenuProps) {
             <p>{`Reach ${POINTS_TO_WIN.toLocaleString()} points to win`}</p>
             <div className="cmmMenuText">
                 <span>How Many teams will be playing?</span><span style={{ paddingLeft: "2px", paddingRight: "2px" }}>-</span><b style={{ paddingRight: "3px", letterSpacing: "-1px" }}>{`${numTeams > 1 ? `${numTeams} Teams` : "Solo Game"}`}</b>
-                <span>
-                    <button onClick={() => setNumTeams(numTeams >= MAX_TEAMS ? MAX_TEAMS : numTeams + 1)}>^</button><button onClick={() => setNumTeams(numTeams <= 1 ? 1 : numTeams - 1)}>&#8964;</button>
-                </span>
             </div>
+            <select className="teamsSelect" value={numTeams} onChange={handleTeamsChange}>
+                {teamOptions}
+            </select>
             <div style={{ position: "relative", top: "30px" }}>
                 <button className="toMainMenuButton toMenuButtons" onClick={() => props.handleChangeSceneButtonClick(SceneDict.MAIN_MENU)}>Back to Menu</button>
                 <button onClick={() => { props.setTeams(createTeams(numTeams)); props.handleChangeSceneButtonClick(SceneDict.CLASSIC_GAME) }}>PLAY</button>
